096701624b32047dfc50c3b7a1dee560f502e4fd
Craig Sturgis <craig@csturg.is>
2015-09-12 15:28:19 -0400

    add strong params to new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(params.require(:article).permit(:title, :text))
 
     @article.save
     redirect_to @article
-
   end
 end
